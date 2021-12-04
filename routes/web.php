84299388ac88cc461f3ac0afe7012ea08282baf2
LeoLopeso <leonardo.lopes@corpstek.com.br>
2021-12-04 12:34:45 -0300

    rotas

--- routes/web.php
+++ routes/web.php
@@ -21,18 +21,45 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 Route::view('/teste', 'teste');
 
-Route::get('/noticia/{slug}', function($slug) {
-    echo "Slug: " . $slug;
+Route::prefix('/config')->group(function() {
+
+    Route::get('/', function() {
+        return view('config');
+    });
+
+    Route::get('info', function() {
+        echo "Configurações INFO 2";
+    });
+
+    Route::get('permissoes', function() {
+        echo "Configurações PERMISSÕES 2";
+    });
+
 });
 
-Route::get('/noticia/{slug}/comentario/{id}', function($slug, $id) {
-    echo "Mostrando o comentário ".$id." da notícia ".$slug;
+Route::fallback(function() {
+    return view('404');
 });
 
-Route::get('/user/{name}', function($name) {
-    echo "Mostrando usuário de Nome: ".$name;
-})->where('name', '[a-z]+');
 
-Route::get('/user/{id}', function($id) {
-    echo "Mostrando usuário ID: ".$id;
-})->where('id', '[0-9]+');
+    // name to routes
+// Route::get('/config/permissoes', function() {
+//     echo "Configurações PERMISSÕES 2";
+// })->name('permissoes');
+
+// Route::get('/noticia/{slug}', function($slug) {
+//     echo "Slug: " . $slug;
+// });
+
+// Route::get('/noticia/{slug}/comentario/{id}', function($slug, $id) {
+//     echo "Mostrando o comentário ".$id." da notícia ".$slug;
+// });
+
+    // routes with validation
+// Route::get('/user/{name}', function($name) {
+//     echo "Mostrando usuário de Nome: ".$name;
+// })->where('name', '[a-z]+');
+
+// Route::get('/user/{id}', function($id) {
+//     echo "Mostrando usuário ID: ".$id;
+// })->where('id', '[0-9]+');
