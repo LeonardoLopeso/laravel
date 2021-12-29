515a4b6685da9bee454b58e40bfa1aade8988ce6
LeoLopeso <leonardo.lopes@corpstek.com.br>
2021-12-29 00:15:31 -0300

    Implementando métodos readeAllTodo, readTodo, update e delete

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -34,20 +34,74 @@ class ApiController extends Controller
         return $array;
     }
 
-    public function readAllTodo() {
+    public function readAllTodos() {
+        $array = ['error' => ''];
 
+        $array['list'] = Todo::all();
+
+        return $array;
     }
 
-    public function readTodo() {
+    public function readTodo($id) {
+        $array = ['error' => ''];
 
+        $todo = Todo::find($id);
+
+        if($todo) {
+            $array['todo'] = $todo;
+        }else{
+            $array['error'] = "A tarefa ".$id." não existe";
+        }
+
+        return $array;
     }
 
-    public function updateTodo() {
+    public function updateTodo($id, Request $request) {
+        $array = ['error' => ''];
 
+        // Validando
+        $rules = [
+            'title' => 'min:3',
+            'done' => 'boolean'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()) {
+            $array['error'] = $validator->messages();
+            return $array;
+        }
+
+        $title = $request->input('title');
+        $done = $request->input('done');
+
+        // Atualizando o item
+        $todo = Todo::find($id);
+
+        if($todo) {
+
+            if($title) {
+                $todo->title = $title;
+            }
+            if($done !== NULL) {
+                $todo->done = $done;
+            }
+
+            $todo->save();
+
+        }else{
+            $array['error'] = 'Tarefa'.$id.' não existe, logo, não pode ser atualizado';
+;        }
+
+        return $array;
     }
 
-    public function deleteTodo() {
+    public function deleteTodo($id) {
+        $array = ['error' => ''];
 
+        $todo = Todo::find($id);
+        $todo->delete();
+
+        return $array;
     }
 
 }
