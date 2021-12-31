a274a199c0a109b78e6901d8bc1351eec0b64816
LeoLopeso <leonardo.lopes@corpstek.com.br>
2021-12-31 13:33:40 -0300

    Upload de arquivo numa api com Laravel 8

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/upload', function(Request $request) {
+    $array = ['error' => ''];
+
+    $rules = [
+        'name' => 'required|min:2',
+        'foto' => 'required|mimes:jpg,png'
+    ];
+
+    $validator = Validator::make($request->all(), $rules);
+
+    if($validator->fails()) {
+        $array['error'] = $validator->messages();
+        return $array;
+    }
+
+    if($request->hasFile('foto')) {
+        if($request->file('foto')->isValid()) {
+
+            $foto = $request->file('foto')->store('public');
+
+            $url = asset(Storage::url($foto));
+
+            $array['url'] = $url;
+
+        }
+
+    }else{
+        $array['error'] = 'Não foi enviado o arquivo';
+    }
+
+    return $array;
 });
